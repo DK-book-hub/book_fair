f5f9ac6dd31225df27f2ea918e93a4020611cfac
山岸大輝 <yamagishidaiki@DK-PC.local>
2025-06-18 16:05:41 +0900

    UI改善：レスポンシブ調整完了

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {},
+    screens: {
+      'xs': '320px', // ← ここがポイント！
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+  },
+  plugins: [],
+}
+
